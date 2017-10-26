834a8784b84c0a450b28d1889a88bdf2c886d1a8
yonggoonoh <nohyg90@gmail.com>
2017-10-27 00:27:29 +0900

    Add eslint to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 
 gulp.task('default', function() {
+    // starts eslint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
+
     // node source
     gulp.src("es6/**/*.js")
         .pipe(babel())
